c562839786220f306d50cd1f4fac686c03c83400
jaoc12 <chitu.stefan@yahoo.com>
2021-04-18 18:52:10 +0300

    Adaugat poze orase

--- app/src/main/java/ro/example/proiect/CitiesAdapter.java
+++ app/src/main/java/ro/example/proiect/CitiesAdapter.java
@@ -1,8 +1,12 @@
 package ro.example.proiect;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,17 +26,28 @@ public class CitiesAdapter extends RecyclerView.Adapter<CitiesAdapter.CityViewHo
     public class CityViewHolder extends RecyclerView.ViewHolder{
         public TextView nameTextView;
         public TextView countryTextView;
+        public ImageView imageView;
+        Resources res;
+        Context context;
 
         public CityViewHolder(View itemView){
             super(itemView);
 
             nameTextView = (TextView) itemView.findViewById(R.id.city_name);
             countryTextView = (TextView) itemView.findViewById(R.id.city_country);
+            imageView = (ImageView) itemView.findViewById(R.id.city_image);
+            res = itemView.getResources();
+            context = itemView.getContext();
         }
 
         public void bind(CityModel cityModel) {
             nameTextView.setText(cityModel.getName());
             countryTextView.setText(cityModel.getCountry());
+
+            String uri = "@drawable/"+cityModel.getName().toLowerCase();
+            int id = res.getIdentifier(uri, null, context.getPackageName());
+            Drawable drawable = res.getDrawable(id, context.getTheme());
+            imageView.setImageDrawable(drawable);
         }
     }
 
--- app/src/main/java/ro/example/proiect/FirstFragment.java
+++ app/src/main/java/ro/example/proiect/FirstFragment.java
@@ -6,6 +6,7 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -41,6 +42,7 @@ public class FirstFragment extends Fragment implements CityOperations {
 
         CitiesAdapter adapter = new CitiesAdapter(citiesList);
         rv.setAdapter(adapter);
+        rv.addItemDecoration(new DividerItemDecoration(rv.getContext(), DividerItemDecoration.VERTICAL));
         rv.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 }
